53130d5d10553d43c1265df319e9f0737d4d7525
Snowvita <138800453+Snowvita@users.noreply.github.com>
2024-11-11 18:26:51 +0530

    Update FlightBookings.jsx

--- client/src/pages/FlightBookings.jsx
+++ client/src/pages/FlightBookings.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import Footer from './Footer';
-
 const FlightBookings = () => {
   const [userDetails, setUserDetails] = useState();
 
